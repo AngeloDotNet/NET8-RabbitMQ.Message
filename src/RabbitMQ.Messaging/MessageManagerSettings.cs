09af3af597435d170387c0667cd685ce7844a343
Angelo Pirola <angelo9871@gmail.com>
2024-07-27 14:57:31 +0200

    Aggiunta configurazione RabbitMQ

---
+++ src/RabbitMQ.Messaging/MessageManagerSettings.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Messaging.Abstractions;
+using System.Text.Json;
+
+namespace RabbitMQ.Messaging;
+
+public class MessageManagerSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string ExchangeName { get; set; } = null!;
+
+    public ushort QueuePrefetchCount { get; set; }
+
+    public JsonSerializerOptions JsonSerializerOptions { get; set; } = JsonOptions.Default;
+}
