40656e943ce602d597ad2ff406db490564040581
Angelo Pirola <angelo9871@gmail.com>
2026-01-14 00:02:03 +0100

    Remove unused properties from MessageManagerSettings
    ExchangeName and QueuePrefetchCount were removed from the MessageManagerSettings class. Also reordered using directives for consistency.

--- src/RabbitMQ.Messaging/MessageManagerSettings.cs
+++ src/RabbitMQ.Messaging/MessageManagerSettings.cs
@@ -1,15 +1,12 @@
-﻿using RabbitMQ.Messaging.Abstractions;
-using System.Text.Json;
+﻿using System.Text.Json;
+using RabbitMQ.Messaging.Abstractions;
 
 namespace RabbitMQ.Messaging;
 
 public class MessageManagerSettings
 {
     public string ConnectionString { get; set; } = null!;
-
     public string ExchangeName { get; set; } = null!;
-
     public ushort QueuePrefetchCount { get; set; }
-
     public JsonSerializerOptions JsonSerializerOptions { get; set; } = JsonOptions.Default;
 }
